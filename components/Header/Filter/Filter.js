eee037b6661f906e23ade1233e87821ee5d880d6
Joey Schrader <jschrader@overstock.com>
2020-11-14 05:00:03 -0800

    clean up & add responsive design to filter

--- components/Header/Filter/Filter.js
+++ components/Header/Filter/Filter.js
@@ -8,7 +8,7 @@ import css from './Filter.module.css'
 
 export default function Filter() {
     return (
-        <div className={css.filter}>
+        <div>
             <CategoryFilter />
             <AreaFilter />
         </div>
@@ -19,19 +19,21 @@ export default function Filter() {
 function CategoryFilter() {
     const { handleFetchCategory, categoryData: { meals } } = useContext(HomeContext)
     return (
-        <div className={css.subFilter}>
+        <div>
             <h3 className={css.title}>Search by Category</h3>
-            {meals.map((meal) => {
-                return (
-                    <button
-                        className={css.btn}
-                        onClick={() => handleFetchCategory(meal.strCategory)}
-                        key={meal.strCategory}
-                    >
-                        {meal.strCategory}
-                    </button>
-                )
-            })}
+            <div className={css.subFilter}>
+                {meals.map((meal) => {
+                    return (
+                        <button
+                            className={css.btn}
+                            onClick={() => handleFetchCategory(meal.strCategory)}
+                            key={meal.strCategory}
+                        >
+                            {meal.strCategory}
+                        </button>
+                    )
+                })}
+            </div>
         </div>
     )
 }
@@ -39,19 +41,21 @@ function CategoryFilter() {
 function AreaFilter() {
     const { handleFetchArea, areaData: { meals } } = useContext(HomeContext)
     return (
-        <div className={css.subFilter}>
+        <div>
             <h3 className={css.title}>Search by Area</h3>
-            {meals.map((mealArea) => {
-                return (
-                    <button
-                        className={css.btn}
-                        onClick={() => handleFetchArea(mealArea.strArea)}
-                        key={mealArea.strArea}
-                    >
-                        {mealArea.strArea}
-                    </button>
-                )
-            })}
+            <div className={css.subFilter}>
+                {meals.map((mealArea) => {
+                    return (
+                        <button
+                            className={css.btn}
+                            onClick={() => handleFetchArea(mealArea.strArea)}
+                            key={mealArea.strArea}
+                        >
+                            {mealArea.strArea}
+                        </button>
+                    )
+                })}
+            </div>
         </div>
     )
 }
